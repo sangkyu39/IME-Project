e5bd9d8fa5d069fc77006b47acb2424782b371bc
sangkyu <doesol1301@gmail.com>
2024-01-04 15:33:24 +0900

    adminMypage 체크박스 수정

--- src/routes/AdminMypage.js
+++ src/routes/AdminMypage.js
@@ -7,16 +7,34 @@ import search from "../assets/search.svg";
 import downloadCoud from "../assets/download-cloud.svg";
 import download from "../assets/download.svg";
 import uploadCloud from "../assets/upload-cloud.svg";
+import downloadCloudGray from "../assets/download-cloud-gray.svg";
+import upArrow from "../assets/up_arrow.svg";
+import downArrow from "../assets/down_arrow.svg";
 
 function AdminMypage() {
+	// const downloadLink = "srcassetsstudentForm.xlsx";
 	const [major, setMajor] = useState("시각디자인학과");
 	const [studentInfo, setStudentInfo] = useState([
-		{ name: "조예린", id: "19011722", num: "12", state: "재학", pay: true, admin: false },
+		{ name: "조예린", id: "19011721", num: "12", state: "재학", pay: true, admin: false },
 		{ name: "조예린", id: "19011722", num: "12", state: "재학", pay: false, admin: true },
-		{ name: "조예린", id: "19011722", num: "12", state: "재학", pay: true, admin: false },
-		{ name: "조예린", id: "19011722", num: "12", state: "재학", pay: true, admin: true },
-		{ name: "조예린", id: "19011722", num: "12", state: "재학", pay: true, admin: false },
+		{ name: "조예린", id: "19011723", num: "12", state: "재학", pay: true, admin: false },
+		{ name: "조예린", id: "19011724", num: "12", state: "재학", pay: true, admin: true },
+		{ name: "조예린", id: "19011725", num: "12", state: "재학", pay: true, admin: false },
 	]);
+	const [idAsc, setIdAsc] = useState(true);
+	const [searchId, setSearchId] = useState("");
+
+	const handleAsc = () => {
+		const copyInfo = [...studentInfo].sort((a, b) => {
+			if (idAsc) {
+				return a.id.localeCompare(b.id);
+			} else {
+				return b.id.localeCompare(a.id);
+			}
+		});
+		setStudentInfo(copyInfo);
+	};
+
 	return (
 		<MyPageStyled>
 			<Sidebar />
@@ -46,15 +64,36 @@ function AdminMypage() {
 								marginBottom: "1rem",
 							}}>
 							<div class="search-input">
-								<input type="text" placeholder="학생 검색" />
+								<input
+									type="text"
+									placeholder="학생 검색"
+									value={searchId}
+									onChange={(e) => {
+										setSearchId(e.value);
+									}}
+								/>
 								<img class="search-icon" src={search} alt="돋보기 아이콘" />
 							</div>
 							<div className="dataload">
-								<img src={uploadCloud} alt="uploadCloud" />
-								<span>데이터 내보내기</span>
+								{studentInfo ? (
+									<>
+										<img src={downloadCoud} alt="downloadCloud" />
+										<span>데이터 내보내기</span>
+									</>
+								) : (
+									<>
+										<img src={downloadCloudGray} alt="uploadCloudGray" />
+										<span
+											style={{
+												color: "var(--grayscale-300, #A3AED0)",
+											}}>
+											데이터 내보내기
+										</span>
+									</>
+								)}
 							</div>
 							<div className="dataload">
-								<img src={downloadCoud} alt="downloadCloud" />
+								<img src={uploadCloud} alt="uploadCloud" />
 								<span>데이터 가져오기</span>
 							</div>
 							<div className="dataload">
@@ -65,7 +104,16 @@ function AdminMypage() {
 						<table className="infoTable">
 							{/* 목록 */}
 							<th>이름</th>
-							<th>학번</th>
+							<th>
+								<span
+									onClick={() => {
+										setIdAsc(!idAsc);
+										handleAsc();
+									}}>
+									학번
+								</span>
+								<img src={idAsc ? upArrow : downArrow} alt="arrow" />
+							</th>
 							<th>사물함 번호</th>
 							<th>상태</th>
 							<th>학생회비 납부</th>
@@ -74,13 +122,41 @@ function AdminMypage() {
 							{studentInfo ? (
 								studentInfo.map(function (info, i) {
 									return (
-										<tr>
+										<tr
+											style={{
+												borderBottom: "1px solid var(--background, #F4F7FE)",
+											}}
+											key={i}>
 											<td>{info.name}</td>
 											<td>{info.id}</td>
 											<td>{info.num}</td>
 											<td>{info.state}</td>
-											<td>{info.pay ? "O" : "X"}</td>
-											<td>{info.admin ? "O" : "X"}</td>
+											<td>
+												<input
+													type="checkbox"
+													id={`pay${i}`}
+													checked={info.pay}
+													onClick={() => {
+														let copyInfo = [...studentInfo];
+														copyInfo[i].pay = !copyInfo[i].pay;
+														setStudentInfo(copyInfo);
+													}}
+												/>
+												<label for={`pay${i}`}></label>
+											</td>
+											<td>
+												<input
+													type="checkbox"
+													id={`admin${i}`}
+													checked={info.admin}
+													onClick={() => {
+														let copyInfo = [...studentInfo];
+														copyInfo[i].admin = !copyInfo[i].admin;
+														setStudentInfo(copyInfo);
+													}}
+												/>
+												<label for={`admin${i}`}></label>
+											</td>
 										</tr>
 									);
 								})
